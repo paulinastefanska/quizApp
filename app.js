bf91fb8dae80ac781d6eefb4ff437a00409782d9
Paulina Stefanska <web.paulinastefanska@gmail.com>
2019-12-13 22:34:36 +0100

    Import path and game.js

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 // import express from "express";
 const express = require("express");
+const path = require("path");
+const gameRoutes = require("./routes/game");
 const app = express();
 
 app.listen(3000, () => {
   console.log("Server is listening at http://localhost:3000/");
 });
+
+app.use(express.static(path.join(__dirname, "public")));
+
+gameRoutes(app);
